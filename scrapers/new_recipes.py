dc1837b7a7385bfa647bccef202282d067f7f8fb
Melissa Terry <mkterry@ucsb.edu>
2024-01-18 14:09:48 -0800

    added bongeats.com to new_recipes.py, altered concatination_urls

--- scrapers/new_recipes.py
+++ scrapers/new_recipes.py
@@ -19,16 +19,22 @@ def get_new_urls(random_sleeps=True, lower_sleep=2, upper_sleep=5):
         'accept-language': 'en-US,en;q=0.9,de;q=0.5'
     }
 
-    # Base URLs with a list of recipes on the page
-    base_urls = [
-        "https://www.allrecipes.com/recipes/22908/everyday-cooking/special-collections/new/", # Not updated often
-        "https://food52.com/recipes/newest", # Updated daily
-        "https://cooking.nytimes.com/68861692-nyt-cooking/32998034-our-newest-recipes", # Updated every 2-3 days
-        "https://www.hellofresh.com/recipes/most-recent-recipes", # Updated weekly?
-        "http://www.afghankitchenrecipes.com/recent-recipes/", # Not updated often
-        "https://www.baking-sense.com/all-blog-posts/", # New posts 2-3 times per month
-        "https://www.bbc.co.uk/food/seasons/" # List of recipes by month
+    # List of base URLs with their corresponding patterns
+    url_patterns = [
+        ("https://www.allrecipes.com/recipes/22908/everyday-cooking/special-collections/new/", re.compile(r'https://www\.allrecipes\.com/recipe/\d+/\w+(?:-\w+)+/')),
+        ("https://food52.com/recipes/newest", re.compile(r'/recipes/([^/]+)')),
+        ("https://cooking.nytimes.com/68861692-nyt-cooking/32998034-our-newest-recipes", re.compile(r'/recipes/\d+-[a-z-]+')),
+        ("https://www.hellofresh.com/recipes/most-recent-recipes", re.compile(r'https://www\.hellofresh\.com/recipes/[\d\w-]+-\w{24}')),
+        ("http://www.afghankitchenrecipes.com/recent-recipes/", re.compile(r'http://www\.afghankitchenrecipes\.com/recipe/[\w-]+/')),
+        ("https://www.baking-sense.com/all-blog-posts/", re.compile(r'https://www\.baking-sense\.com/\d+/\d+/\d+/[\w-]+/')), 
+        ("https://www.bongeats.com/recipes", re.compile(r'/recipe/([\w-]+)'))
     ]
+
+    concatenation_urls = {
+        "https://food52.com/recipes/newest": "https://www.food52.com{}",
+        "https://cooking.nytimes.com/68861692-nyt-cooking/32998034-our-newest-recipes": "https://cooking.nytimes.com{}", 
+        "https://www.bongeats.com/recipes": "https://www.bongeats.com{}",
+    }
 
     if random_sleeps:
         sleep_time = random.uniform(lower_sleep, upper_sleep)
@@ -36,39 +42,20 @@ def get_new_urls(random_sleeps=True, lower_sleep=2, upper_sleep=5):
 
     filtered_urls = []
 
-    # Define the patterns using regular expressions
-    allrecipes_pattern = re.compile(r'https://www\.allrecipes\.com/recipe/\d+/\w+(?:-\w+)+/')
-    food52_pattern = re.compile(r'/recipes/([^/]+)')
-    nytimes_pattern = re.compile(r'/recipes/\d+-[a-z-]+')
-    hellofresh_pattern = re.compile(r'https://www\.hellofresh\.com/recipes/[\d\w-]+-\w{24}')
-    afghankitchen_pattern = re.compile(r'http://www\.afghankitchenrecipes\.com/recipe/[\w-]+/')
-    bakingsense_pattern = re.compile(r'https://www\.baking-sense\.com/\d+/\d+/\d+/[\w-]+/')
-
-    for base_url in base_urls:
+    for base_url, pattern in url_patterns:
         html = httpx.get(url=base_url, headers=headers).content
         soup = BeautifulSoup(html, "html.parser")
 
         # Find all <a> tags with a 'href' attribute
         links = [a["href"] for a in soup.find_all("a", href=True)]
 
-        if "allrecipes" in base_url:
-            # Filter out non-recipe links for All Recipes
-            recipe_links = [url for url in links if allrecipes_pattern.match(url)]
-        elif "food52" in base_url:
-            # Filter out non-recipe links for Food 52 and concatenate the string
-            recipe_links = [f"https://www.food52.com{url}" for url in links if food52_pattern.match(url)]
-        elif "nytimes" in base_url:
-            # Filter out non-recipe links for NY Times and concatenate the string
-            recipe_links = [f"https://cooking.nytimes.com{url}" for url in links if nytimes_pattern.match(url)]
-        elif "hellofresh" in base_url:
-            # Filter out non-recipe links for Hello Fresh
-            recipe_links = [url for url in links if hellofresh_pattern.match(url)]
-        elif "afghankitchenrecipes" in base_url:
-            # Filter out non-recipe links for Afghan Kitchen Recipes
-            recipe_links = [url for url in links if afghankitchen_pattern.match(url)]
-        elif "baking-sense" in base_url:
-            # Filter out non-recipe links for All Recipes
-            recipe_links = [url for url in links if bakingsense_pattern.match(url)]
+        # Apply pattern-based filtering
+        recipe_links = [url if concatenation_urls.get(base_url) is None else concatenation_urls[base_url].format(url) for url in links if pattern.match(url)]
+
+        # # Print relevant information for debugging
+        # print(f"Base URL: {base_url}")
+        # print(f"Concatenation URL: {concatenation_urls.get(base_url)}")
+        # print(f"Filtered Recipe Links: {recipe_links}")
 
         # Add the filtered links to the result
         filtered_urls.extend(recipe_links)
